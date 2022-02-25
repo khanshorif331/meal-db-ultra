7933683f8360997d00d6274faca4fc3bb33ebcb8
khanshorif331 <khanshorif331@gmail.com>
2022-02-25 21:51:27 +0600

    error handling done

--- js/meal.js
+++ js/meal.js
@@ -1,32 +1,84 @@
 const searchFood = ()=>{
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value
-    // console.log(searchText);
-    searchField.innerText=''
+    
+    // clear data
+    searchField.value=''
 
+    // error handling with if else
+    if(searchText == ''){
+        alert('Plz enter someting to search')
+    }
+    else{
+
+         // load data
     const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
     `
     fetch(url)
     .then(res=>res.json())
     .then(data=>displaySearchResult(data.meals))
-
+    }
 }
 
+
 const displaySearchResult = meals => {
-    const searchResult = document.getElementById('search-result')
-    meals.forEach(meal =>{
-        // console.log(meal);
-        const div=document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML =`
-        <div class="card h-100">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
-            </div>
-          </div>
+    console.log(meals);
+    if(meals == null){
+        alert('Food not available.Plzz enter something else')
+    }
+    else{
+        const searchResult = document.getElementById('search-result')
+        // searchResult.innerHTML = ''
+        searchResult.textContent = ''
+        if(meals.length == 0){
+        console.log(meals.length);
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <p>Plz search something else</p>
         `
         searchResult.appendChild(div)
-    })
+        }
+        else{
+            meals.forEach(meal =>{
+            // console.log(meal);
+            const div=document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML =`
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+                </div>
+              </div>
+            `
+            searchResult.appendChild(div)
+        })
+    }
+    }    
+}
+
+
+const loadMealDetail = mealId =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=> displayMealDetail(data.meals[0]))
+}
+
+
+const displayMealDetail = meal=>{
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details')
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML =  `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions}</p>
+          <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `
+    mealDetails.appendChild(div)
 }
